42f089f34f41d69acb47910be0595e81e76f980d
Abigail Fernandes <afernandes@cimpress.com>
2018-03-20 01:56:35 +0530

    Feature-Header: Add Hero, Navbar and Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="container">
+      <div className="content has-text-centered">
+        <p>
+          <strong>Built with love by</strong>
+          <a href="https://github.com/sanchitgn"> Sanchit</a> and{' '}
+          <a href="https://github.com/AbigailFernandes">Abigail</a>. The source
+          code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The
+          website content is licensed{' '}
+          <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+            CC BY NC SA 4.0
+          </a>.
+        </p>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import './styles.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Shikha's Studio" />
+    <Navbar />
     <div>{children()}</div>
+    <Footer />
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
