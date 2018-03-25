61ae5512d3cdcfa7afdf3d05044bde9587a26635
Abigail Fernandes <afernandes@cimpress.com>
2018-03-25 18:17:57 +0530

    Feature/Testimonial: Add index.js for images displayed on the home page

--- src/components/HeroCarousel.js
+++ src/components/HeroCarousel.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import Carousel from 'nuka-carousel';
-import home1 from '../img/home/home-1.jpg';
-import home2 from '../img/home/home-2.jpg';
-import home3 from '../img/home/home-3.jpg';
-import home4 from '../img/home/home-4.jpg';
-import home5 from '../img/home/home-5.jpg';
+import * as home from '../img/home/';
 
 const images = [
-  { src: home1 },
-  { src: home2 },
-  { src: home3 },
-  { src: home4 },
-  { src: home5 },
+  { src: home.home1 },
+  { src: home.home2 },
+  { src: home.home3 },
+  { src: home.home4 },
+  { src: home.home5 },
 ];
 
 const HeroCarousel = () => (
---
+++ src/img/home/index.js
@@ -0,0 +1,5 @@
+export { default as home1 } from './home-1.jpg';
+export { default as home2 } from './home-2.jpg';
+export { default as home3 } from './home-3.jpg';
+export { default as home4 } from './home-4.jpg';
+export { default as home5 } from './home-5.jpg';
