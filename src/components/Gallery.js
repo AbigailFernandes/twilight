921732ac0b1c7a25f8f37ea71862d4d798f4cc6a
Sanchit Nevgi <sanchit.nevgi@olacabs.com>
2018-03-25 15:21:56 +0530

    Iterate over albums in Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import Album from '../components/Album';
 
+const albums = [
+  { title: 'Student Portfolio', artwork: 'http://www.jharnashah.com/wp-content/gallery/spa-and-salon/4..jpg' },
+  { title: 'Spa and Salon', artwork: 'http://www.jharnashah.com/wp-content/gallery/spa-and-salon/3.jpg' },
+  { title: 'Jharna shah senior assistants' },
+  { title: 'Jharna shah junior make up artist' },
+  { title: 'Hair Styling' },
+  { title: 'Personal (Self) grooming Workshops' },
+  { title: 'Classroom Dairies' },
+  { title: 'Jharna Shah' },
+]
+
 export default () => (
   <div className="gallery-container">
     <h1 className='title is-4'>Gallery</h1>
     <h2 className='subtitle is-5'>Featured Albums</h2>
     <div className='columns is-multiline albums'>
-      <Album title='Student Portfolio' artwork='http://www.jharnashah.com/wp-content/gallery/spa-and-salon/4..jpg' />
-      <Album title='Spa and Salon' artwork='http://www.jharnashah.com/wp-content/gallery/spa-and-salon/3.jpg' />
-      <Album title='Jharna shah senior assistants' />
-      <Album title='Jharna shah junior make up artist' />
-      <Album title='Hair Styling' />
-      <Album title='Personal (Self) grooming Workshops' />
-      <Album title='Classroom Dairies' />
-      <Album title='Jharna Shah' />
+      {
+        albums.map(album => <Album {...album} />)
+      }
     </div>
   </div>
 );
