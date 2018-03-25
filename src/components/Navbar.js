49032e295a39821fb6e3debb616e578d4e22410a
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2018-03-25 19:02:49 +0530

    Use Link in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,34 +2,34 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 const Navbar = () => (
-  <nav className="navbar is-primary">
-    <div className="container">
-      <div className="navbar-brand">
-        <span className="navbar-burger burger" data-target="navbarMenuHeroA">
+  <nav className='navbar is-primary'>
+    <div className='container'>
+      <div className='navbar-brand'>
+        <span className='navbar-burger burger' data-target='navbarMenuHeroA'>
           <span />
           <span />
           <span />
         </span>
       </div>
-      <div id="navbarMenuHeroA" className="navbar-menu">
-        <div className="navbar-end">
-          <Link className="navbar-item is-active" to="/">
+      <div id='navbarMenuHeroA' className='navbar-menu'>
+        <div className='navbar-end'>
+          <Link className='navbar-item is-active' to='/'>
             Home
           </Link>
-          <a className="navbar-item">About</a>
-          <a className="navbar-item">Courses</a>
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">Services</a>
-            <div className="navbar-dropdown is-boxed is-primary">
-              <a className="dropdown-item">Bridal Makeup</a>
-              <a className="dropdown-item">TV Commerical</a>
-              <a className="dropdown-item">Special Occasions</a>
+          <Link className='navbar-item' to='/about'>About</Link>
+          <Link className='navbar-item' to='/courses'>Courses</Link>
+          <div className='navbar-item has-dropdown is-hoverable'>
+            <Link className='navbar-link' to='/services'>Services</Link>
+            <div className='navbar-dropdown is-boxed is-primary'>
+              <Link className='dropdown-item'>Bridal Makeup</Link>
+              <Link className='dropdown-item'>TV Commerical</Link>
+              <Link className='dropdown-item'>Special Occasions</Link>
             </div>
           </div>
-          <Link className="navbar-item" to="/gallery">
+          <Link className='navbar-item' to='/gallery'>
             Gallery
           </Link>
-          <Link className="navbar-item" to="/contact">
+          <Link className='navbar-item' to='/contact'>
             Contact Us
           </Link>
         </div>
