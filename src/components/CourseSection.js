38c37796302c908811390afaaba4152c08e44376
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2018-04-01 13:35:16 +0530

    Make CourseSection responsive

--- src/components/CourseSection.js
+++ src/components/CourseSection.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { home1 } from '../img/home';
 import '../stylesheets/courses.scss';
 
-export default () => (
+export default ({ image = home1 }) => (
   <div className="container columns course-section">
-    <div class="column is-one-quarter" />
-    <div className="course-photo column is-one-third ">
-      <figure className="walden">
-        <img src={home1} />
+    <div class="column is-2-desktop is-hidden-touch" />
+    <div className="course-photo column is-one-third-desktop is-half-tablet">
+      <figure className="aden">
+        <img className="course-image" src={image} />
       </figure>
     </div>
-    <div className="course-list column is-one-quarter content">
-      <h2 className="is-size-3 has-text-grey">
+    <div className="course-list column is-one-third-desktop is-half-tablet content">
+      <span className="is-size-3 has-text-grey">
         Professional Diploma in Makeup & Hair Styling
-      </h2>
+      </span>
       <ul>
         <li>14 Weeks Comprehensive Diploma in Makeup & Hairstyling </li>
         <li>9 Weeks Fast Track Diploma in Makeup & Hairstyling </li>
--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { CourseSection } from '../components';
+import { home1, home2 } from '../img/home';
 
 export default () => (
   <div className='section'>
     <h1 className='is-size-2 has-text-centered'>Courses</h1>
-    <CourseSection />
-    <CourseSection />
+    <CourseSection image={home1} />
+    <CourseSection image={home2} />
   </div>
 )
