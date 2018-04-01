ea49e815130789cf11b311ea635e8f81f5c8cd8d
Abigail Fernandes <abigailferns94@gmail.com>
2018-04-01 18:17:55 +0530

    Feature/Services: Use Parallax for page header (#18)

---
+++ src/components/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../stylesheets/services.scss';
+import bridal1 from '../img/home/';
+
+export default ({ url }) => (
+  <section className="section section-page-title">
+    <div className="overlay has-text-centered">
+      <h1 className="title is-1" style={{ color: 'white' }}>
+        Makeup For the Bride
+      </h1>
+    </div>
+  </section>
+);
--- src/img/home/index.js
+++ src/img/home/index.js
@@ -3,13 +3,17 @@ import { default as home2 } from './home-2.jpg';
 import { default as home3 } from './home-3.jpg';
 import { default as home4 } from './home-4.jpg';
 import { default as home5 } from './home-5.jpg';
+import { default as bridal1 } from './bridal-1.jpg';
+import { default as bridal2 } from './bridal-2.jpg';
 
-export { home1, home2, home3, home4, home5 };
+export { home1, home2, home3, home4, home5, bridal1, bridal2 };
 
 export const images = [
-  { src: home1},
-  { src: home2},
-  { src: home3},
-  { src: home4},
-  { src: home5}
+  { src: home1 },
+  { src: home2 },
+  { src: home3 },
+  { src: home4 },
+  { src: home5 },
+  { src: bridal1 },
+  { src: bridal2 },
 ];
