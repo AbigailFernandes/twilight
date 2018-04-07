40016fbc20104f437fd5fe6c475b2dcf5f031c78
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2018-04-07 14:13:42 +0530

    Decouple carousel styles, add carousel controls

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import '../stylesheets/carousel.scss';
 
 class Carousel extends Component {
+  componentDidMount() {}
   render() {
     return (
-      <div className='container is-fluid carousel'>
+      <div className="container is-fluid carousel">
+        <div className="controls-container">
+          <div className="control">&#10094;</div>
+          <div className="control">&#10095;</div>
+        </div>
         {this.props.children}
       </div>
-    )
+    );
   }
 }
 
