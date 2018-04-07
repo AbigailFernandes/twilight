303016f5f43a2a48b82b0e490c218a18f620ca69
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2018-04-07 11:00:57 +0530

    Add About section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default () => (
+  <section className="section about-section">
+    <div className="columns">
+      <div className="column">
+        <img src="http://s3.amazonaws.com/peoplepng/wp-content/uploads/2018/02/24083416/Dentist-Smile-PNG-Transparent-Picture.png" />
+      </div>
+      <div className="column">
+        <span className="is-size-3 has-text-primary has-text-weight-normal">
+          About Shikha
+        </span>
+        <div className="content has-text-justified">
+          <blockquote>
+            Shikha Make up and Hair Academy Boutique Salon Spa commenced with an
+            aspiration and continued with dedication.
+          </blockquote>
+          <p>
+            At present, it is one of the top names in the make up and hair
+            industry. There are a host of combinations that made this academy
+            the most renowned in the beauty circuit- luxurious & trendy
+            amenities, proficient & upgraded professionals, and dynamic
+            improvement approach.
+          </p>
+          <p>
+            {' '}
+            For the newbies aspiring to fly in the skies of beauty industry, we
+            provide a comprehensive professional training in make up and hair.
+          </p>
+          <p>
+            We have proficient counsellors conducting self-grooming workshops,
+            ensuring every woman can avail the benefits.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+);
