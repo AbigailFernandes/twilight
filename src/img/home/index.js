4b110ed57bb9da74d6ff6d153d69b648c759269d
Abigail Fernandes <afernandes@cimpress.com>
2018-04-10 01:30:05 +0530

    Feature/Service: Create Service component for Service Page

--- src/img/home/index.js
+++ src/img/home/index.js
@@ -6,8 +6,23 @@ import { default as home5 } from './home-5.jpg';
 import { default as bridal1 } from './bridal-1.jpg';
 import { default as bridal2 } from './bridal-2.jpg';
 import { default as marilynQuote } from './marilyn-quote.jpg';
+import { default as service1 } from './service1.jpg';
+import { default as service2 } from './service2.jpg';
+import { default as service3 } from './service3.jpg';
 
-export { home1, home2, home3, home4, home5, bridal1, bridal2, marilynQuote };
+export {
+  home1,
+  home2,
+  home3,
+  home4,
+  home5,
+  bridal1,
+  bridal2,
+  marilynQuote,
+  service1,
+  service2,
+  service3,
+};
 
 export const images = [
   { src: home1 },
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import PageHeader from '../components/PageHeader';
+import { ServiceSection, PageHeader } from '../components';
+import { service1, service2, service3 } from '../img/home';
+
+const services = [
+  {
+    image: service1,
+    title: 'Bridal Makeup',
+    peach: true,
+    content:
+      'Considering just how important the makeup arts are for these industries, the quality of my work was required to match the class. After hundreds of sessions it seems it did!',
+  },
+  {
+    image: service2,
+    title: 'TV Film Commercial',
+    cyan: true,
+    content:
+      'Considering just how important the makeup arts are for these industries, the quality of my work was required to match the class. After hundreds of sessions it seems it did!',
+  },
+  {
+    image: service3,
+    title: 'Special Occasions',
+    teal: true,
+    content:
+      'Considering just how important the makeup arts are for these industries, the quality of my work was required to match the class. After hundreds of sessions it seems it did!',
+  },
+];
 
 export default () => (
   <section className="section">
     <PageHeader title="Services" className="section-services-title" />
+    <section className="section">
+      <div className="columns is-centered is-gapless">
+        {services.map(service => (
+          <ServiceSection key={service.title} {...service} />
+        ))}
+      </div>
+    </section>
   </section>
 );
