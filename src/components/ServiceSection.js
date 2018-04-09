df2d0140796798b2d731e1606ec54a817fdbc710
Abigail Fernandes <afernandes@cimpress.com>
2018-04-10 01:40:59 +0530

    Feature/Service: Add button for Read More

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { service1 } from '../img/home';
 import classNames from 'classnames';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faAngleDoubleRight from '@fortawesome/fontawesome-free-solid/faAngleDoubleRight';
 import '../stylesheets/services.scss';
 
 export default ({ content, title, image, peach, cyan, teal }) => (
@@ -18,7 +20,10 @@ export default ({ content, title, image, peach, cyan, teal }) => (
     <div className="service-contents">
       <h1 className="title is-3">{title}</h1>
       <p className="has-text-justified">{content}</p>
-      <a className="button is-dark is-outlined padding">Read More</a>
+      <a className="button is-dark is-outlined padding">
+        Read More &nbsp;
+        <FontAwesomeIcon icon={faAngleDoubleRight} />
+      </a>
     </div>
   </div>
 );
