805544b16c3a8e95c9417088a905787401af5533
Abigail Fernandes <afernandes@cimpress.com>
2018-05-12 22:41:58 +0530

    Feature/Content: Add content

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -2,10 +2,35 @@ import React from 'react';
 import { CourseSection, PageHeader } from '../components';
 import { home1, home2 } from '../img/home';
 
+const courses = [
+  {
+    image: home1,
+    title: 'Bridal Makeup course',
+    reverse: false,
+    content: ['4-5 weeks comprehensive detailed course.'],
+  },
+  {
+    image: home2,
+    title: 'Professional Make-up and Hair Styling',
+    reverse: true,
+    content: [
+      '10-12 weeks comprehensive detailed course in make-up and hairstyling',
+      'Additional week for draping.',
+    ],
+  },
+  {
+    image: home1,
+    title: 'Personal Grooming',
+    reverse: false,
+    content: [
+      'Per day grooming classes based on what the client wants to learn.',
+      'Personal grooming includes basic self-learning make-up sessions, day & night look, party make-up look (smokey eyes) etc.',
+    ],
+  },
+];
 export default () => (
   <div className="section course-page">
     <h1 className="is-size-2 has-text-centered">Courses</h1>
-    <CourseSection image={home1} />
-    <CourseSection image={home2} reverse />
+    {courses.map(course => <CourseSection key={course.title} {...course} />)}
   </div>
 );
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,7 +3,7 @@ import Gallery from '../components/Gallery';
 import PageHeader from '../components/PageHeader';
 
 export default () => (
-  <section className="section">
+  <section className="section  is-paddingless">
     <PageHeader title="Gallery" className="section-gallery-title" />
     <Gallery />
   </section>
