a39426ed9c39cac6a6b5e2152d891d294f9a1c10
Abigail Fernandes <afernandes@cimpress.com>
2018-05-12 22:56:30 +0530

    Feature/Content: justify content for testimonial

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -14,7 +14,7 @@ export default ({ name = 'Juhi Jayaram', review = 'Default' }) => (
     <div className="has-text-centered margin-bottom-2">
       <FontAwesomeIcon icon={faQuoteLeft} size="3x" />
     </div>
-    <p className="has-text-centered margin-bottom-2">{review}</p>
+    <p className="has-text-justified margin-bottom-2">{review}</p>
     <p className="has-text-centered">
       <strong>{name}</strong>
     </p>
