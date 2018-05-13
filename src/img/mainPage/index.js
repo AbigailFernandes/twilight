ab48161568d6530228afe25e51c7a9f4080a3c7f
Abigail Fernandes <afernandes@cimpress.com>
2018-05-14 00:50:23 +0530

    Feature/Photos: Fixed extension

--- src/img/mainPage/index.js
+++ src/img/mainPage/index.js
@@ -1,7 +1,7 @@
 import { default as s1 } from './1.jpeg';
 import { default as s2 } from './2.jpeg';
 import { default as s3 } from './3.jpeg';
-import { default as s5 } from './5.JPG';
+import { default as s5 } from './5.jpg';
 export { s1, s2, s3, s5 };
 
 export const images = [{ src: s1 }, { src: s2 }, { src: s3 }, { src: s5 }];
