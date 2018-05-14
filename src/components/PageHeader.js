43677547109153a3d17b34efb36a01c84a3a0f1e
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2018-05-14 21:42:42 +0530

    Polish PageHeader

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -3,14 +3,9 @@ import '../stylesheets/page-header.scss';
 import bridal1 from '../img/home/';
 
 export default ({
-  title = 'Page Title',
-  className = 'section-services-title',
+  title = 'Page Title'
 }) => (
-  <section className={className}>
-    <div className="overlay has-text-centered">
-      <h1 className="title is-1" style={{ color: 'white' }}>
-        {title}
-      </h1>
-    </div>
-  </section>
+  <div className='page-header'>
+      <span className="header__title bordered-underline">{title}</span>
+  </div>
 );
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,8 +3,8 @@ import PhotoGrid from '../components/PhotoGrid';
 import PageHeader from '../components/PageHeader';
 
 export default () => (
-  <section className="section  is-paddingless">
-    <PageHeader title="Gallery" className="section-gallery-title" />
+  <section className="section  is-paddingless has-text-centered">
+    <PageHeader title="gαℓℓєяу" />
     <container className="container is-fluid">
       <PhotoGrid />
     </container>
