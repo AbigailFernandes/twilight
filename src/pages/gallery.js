2730bfb2ed853ed0beb5543f379abff1055c6cca
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2018-05-14 22:07:38 +0530

    Update Gallery PageHeader

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,8 +3,8 @@ import PhotoGrid from '../components/PhotoGrid';
 import PageHeader from '../components/PageHeader';
 
 export default () => (
-  <section className="section  is-paddingless has-text-centered">
-    <PageHeader title="gαℓℓєяу" />
+  <section className="gallery-section section is-paddingless has-text-centered">
+    <PageHeader title="𝔾𝔸𝕃𝕃𝔼ℝ𝕐" />
     <container className="container is-fluid">
       <PhotoGrid />
     </container>
