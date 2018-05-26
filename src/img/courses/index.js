284d57cda8a2286187ed0fd1e89370fe3a7300b2
Abigail Fernandes <afernandes@cimpress.com>
2018-05-26 14:18:24 +0530

    Rename file as JPG does not work

--- src/img/courses/index.js
+++ src/img/courses/index.js
@@ -1,12 +1,4 @@
-import { default as s1 } from './1.JPG';
-import { default as s2 } from './2.JPG';
-import { default as s3 } from './3.JPG';
+import { default as s1 } from './a1.jpg';
+import { default as s2 } from './a2.jpg';
+import { default as s3 } from './a3.jpg';
 export { s1, s2, s3 };
-
-// export const images = [
-//   { src: s1 },
-//   { src: s2 },
-//   { src: s5 },
-//   { src: s3 },
-//   { src: s6 },
-// ];
